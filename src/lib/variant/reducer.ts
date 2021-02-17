8d7bdb2620c174f94cf1bb9b408f9ce897005ecc
akosfi <fiakos1997@gmail.com>
2021-02-17 08:16:46 +0100

    Extended variant redux.

--- src/lib/variant/reducer.ts
+++ src/lib/variant/reducer.ts
@@ -1,17 +1,20 @@
 import { Reducer } from "redux";
 import { produce } from "immer";
 
-import {Variant} from "~lib/variant/types";
-import {VariantActionConstants} from "~lib/variant/actions";
+import { Variant } from "~lib/variant/types";
+import { VariantActionConstants } from "~lib/variant/actions";
+import { variableDeclaration } from "@babel/types";
 
 export type VariantsState = {
     variants: Variant[];
     isLoading: boolean;
-}
+    error: string;
+};
 
 export const initialState: VariantsState = {
     variants: [],
-    isLoading: false
+    isLoading: false,
+    error: ""
 };
 
 const reducer: Reducer<VariantsState> = (state = initialState, action): VariantsState => {
@@ -19,6 +22,20 @@ const reducer: Reducer<VariantsState> = (state = initialState, action): Variants
         case VariantActionConstants.LOAD_VARIANTS: {
             return produce(state, draft => {
                 draft.isLoading = true;
+            });
+        }
+        case VariantActionConstants.LOAD_VARIANTS_SUCCESS: {
+            return produce(state, draft => {
+                const { variants } = action.payload;
+                draft.isLoading = false;
+                draft.variants = variants;
+            });
+        }
+        case VariantActionConstants.LOAD_VARIANTS_ERROR: {
+            return produce(state, draft => {
+                const { error } = action.payload;
+                draft.isLoading = false;
+                draft.error = error;
             });
         }
         default: {
