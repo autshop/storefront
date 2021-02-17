82bcecc28ce91db566a7f4e88344b73e86cab347
akosfi <fiakos1997@gmail.com>
2021-02-17 19:06:42 +0100

    added types

--- src/lib/variant/types.ts
+++ src/lib/variant/types.ts
@@ -1,14 +1,28 @@
+export type VariantStatus = "active" | "disabled" | "active_on";
+
 export type VariantSize = {
     id: number;
     name: string;
     quantity: string;
-}
+    hasStock: boolean;
+    position: number;
+};
 
 export type Variant = {
     id: number;
     name: string;
+    description: string;
     code: string;
-    availability: string;
+    status: VariantStatus;
+    statusDate: string;
     sizes: VariantSize[];
-}
+    price: number;
+    currencyCode: string;
+    sku: string;
+    barcode: string;
+};
 
+export type Product = {
+    id: number;
+    variants: Variant[];
+};
