5f32855c76d635806ceb0724a396669080a415d9
Akos Fi <fi.akos@straxus.hu>
2021-10-09 10:02:16 +0200

    [fix] Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-while getopts ":u:p:" opt; do
-  case $opt in
-    u) $DOCKERHUB_USERNAME="$OPTARG"
-    ;;
-    p) $DOCKERHUB_PASSWORD="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    ;;
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --u=*)
+      DOCKERHUB_USERNAME="${1#*=}"
+      ;;
+    --p=*)
+      DOCKERHUB_PASSWORD="${1#*=}"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
   esac
+  shift
 done
 
 docker build -t akosfi/storefront -f ./docker/Dockerfile .
